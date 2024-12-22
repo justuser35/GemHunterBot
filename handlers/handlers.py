c4bfbdd2571be9934570e6e159d335c5e2c974d3
justuser35 <danilolenev2007@gmail.com>
2024-12-22 05:12:22 +0300

    primary aiogram setup

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,9 +1,64 @@
 from aiogram import Router
-from aiogram import filters as flt
-from aiogram.types import Message
-
-from Bot import bot
-# import filters
-# from Bot import db
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.filters import Command
 
 router = Router()
+
+
+@router.message(Command("start"))
+async def command_menu(message: Message):
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="Мой профиль"), KeyboardButton(text="Список лидеров"), KeyboardButton(text="Достижения")],
+            [KeyboardButton(text="Перейти в шахту")], # в шахте будут ежедневки и др.
+        ],
+        resize_keyboard=True
+    )
+    await message.answer("тут будет профиль игрока", reply_markup=keyboard)
+
+
+@router.message(Command("help"))
+async def command_help(message: Message):
+    text = '''<a href="https://t.me/GemHunterBot_help">Официальное FAQ</a>
+ ➖➖➖➖➖➖
+Другие вопросы и предложения по улучшению пишите в нашу группу @GemHunterBot_chat'''
+    await message.answer(text, parse_mode="HTML")
+
+
+@router.message(Command("id"))
+async def get_id(message: Message):
+    user_id = message.from_user.id
+    await message.answer(f"Ваш ID: {user_id}")
+
+
+@router.message()
+async def handle_reply_button(message: Message):
+    if message.text == "Достижения":
+        new_keyboard = ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text="⏪Назад"), KeyboardButton(text="Моих достижений: 10")],
+                [KeyboardButton(text="Список достижений и награды")]
+            ],
+            resize_keyboard=True
+        )
+
+    elif message.text == "Список лидеров":
+        await message.answer("Список лидеров")
+
+    elif message.text == "Перейти в шахту":
+        await message.answer("Переход в шахту")
+
+    elif message.text == "⏪Назад":
+        keyboard = ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text="Мой профиль"), KeyboardButton(text="Список лидеров"),
+                 KeyboardButton(text="Достижения")],
+                [KeyboardButton(text="Перейти в шахту")],
+            ],
+            resize_keyboard=True
+        )
+        await message.answer("тут будет профиль игрока", reply_markup=keyboard)
+
+@router.message()
+async def handle_reply_button(message: Message):
+        await message.answer(f"тут будет профиль игрока")
